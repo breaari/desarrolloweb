8e1434ad4e6b6dd7c256f68115f6eee37d3fc716
Ariana <aru.brea@gmail.com>
2024-08-06 23:31:00 -0300

    b

--- src/Components/Contact/contact.jsx
+++ src/Components/Contact/contact.jsx
@@ -3,7 +3,7 @@ import impulsar2 from "../../assets/impulsar2.png"
 
 export const Contact = () => {
     return (
-        <div className="min-h-screen bg-white flex flex-row laputaquetepario justify-center items-center mq980:flex-col">
+        <div className="min-h-screen max-w-screen bg-white flex flex-row laputaquetepario justify-center items-center mq980:flex-col overflow-hidden">
             <div className="relative blue-rounded-tl laputaquetepario bg-white w-[50%] text-5xl text-azuloscuro font-extrabold text-center flex flex-col justify-center items-center px-10 py-10 mq980:w-full mq980:px-5 mq980:text-5xl">
                 Descubrí cómo podemos transformar tu presencia en la web y destacar en el mundo digital
                 <img src={impulsar2} className="w-[200px] mt-4" alt="Impulsar 2" />
