d304c6b8fd5bc296613b3c81c7f9c54f44993fc0
Ariana <aru.brea@gmail.com>
2024-08-11 17:32:45 -0300

    a

--- src/Components/Chat/chat.jsx
+++ src/Components/Chat/chat.jsx
@@ -1,14 +1,3 @@
-// import { FaRocketchat } from "react-icons/fa";
-
-// export const Chat = () => {
-//     return (
-//         <div className="fixed right-5 bottom-5 z-30 shadow-azuloscuro shadow-lg flex items-center justify-center bg-celeste rounded-[70px] p-5">
-//             <FaRocketchat className="text-white text-4xl" />
-//         </div>
-//     )
-// }
-
-
 import React, { useState } from "react";
 import { FaRocketchat } from "react-icons/fa";
 
@@ -20,7 +9,7 @@ export const Chat = () => {
             <a className={`fixed right-5 bottom-5 z-30 shadow-azuloscuro shadow-lg cursor-pointer rounded-[70px] bg-celeste p-5 flex items-center justify-evenly overflow-hidden transition-all duration-300 ${isHovered ? "hover:w-auto px-6" : ""}`}
                 onMouseEnter={() => setIsHovered(true)}
                 onMouseLeave={() => setIsHovered(false)}
-                href='https://api.whatsapp.com/send?phone=5492235512665&text=Hola%21%20Quiero%20m%C3%A1s%20informaci%C3%B3n%20sobre%E2%80%A6'
+                href='https://api.whatsapp.com/send?phone=5491136002250'
                 target="_blank"
                 rel="noopener noreferrer"
             >
